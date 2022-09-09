4a12c01204e77a902d27ed10aff63c5740893500
ifendif2004 <ifendif2004@gmail.com>
2022-09-09 20:31:44 +0200

    Primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Como Conectarse a una API con Javascript usando Async, Await y Fetch
---
+++ app.js
@@ -0,0 +1,92 @@
+var dateControl = document.querySelector('input[type="date"]')
+var geolimit = document.getElementById('sellimit')
+var lista = document.getElementById('lista')
+var btnCalcular = document.getElementById('btnCalcular')
+
+
+btnCalcular.addEventListener("click", (event)=>{
+	event.preventDefault();
+	// console.log('click boton xxx');
+	// console.log(dateControl.value);
+	const startdate = dateControl.value + "T00:00"
+	const enddate = dateControl.value + "T23:59"
+	cargarPrecios(startdate, enddate);
+})
+
+/* userInput.addEventListener('submit', (event) => {
+	event.preventDefault();
+	console.log('click boton');
+	console.log(dateControl.value);
+	const startdate = dateControl.value + "T00:00"
+	const enddate = dateControl.value + "T23:59"
+	cargarPrecios(startdate, enddate);
+}); */
+
+
+const cargarPrecios = async (startdate, enddate) => {
+	try {
+		lista.innerHTML = ''
+		// console.log(geolimit.value)
+		// console.log(startdate)
+		// console.log(enddate)
+		const apirest = `https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date=${startdate}&end_date=${enddate}&geo_limit=${geolimit.value}&time_trunc=hour`
+
+		// console.log(apirest)
+
+
+		const respuesta = await fetch(apirest);
+
+		// console.log(respuesta.status);
+		//		console.log(respuesta);
+
+		// Si la respuesta es correcta
+		if (respuesta.status === 200) {
+			const datos = await respuesta.json();
+			// console.log(datos);
+			// console.log(datos.included[0].attributes.values[0].datetime + ": " + datos.included[0].attributes.values[0].value);
+			let preciosHora = '';
+			//			const datos1 = await datos.included[0].json();
+			//			console.log(datos.include[0].values);
+			datos.included[0].attributes.values.forEach(hora => {
+				//				console.log(hora.datetime.slice(11,16))
+				preciosHora += `
+				<div class="itempreciohora">
+				<img src="./img/verde.png">
+				<span>${hora.datetime.slice(11, 16)} -- ${(hora.value / 1000).toFixed(5)} kWh</span>
+				
+			</div>
+						
+
+				`;
+
+				/* 				preciosHora += `
+									<div class="precio-hora">
+									<p> ==== </p>
+										<p>${hora.datetime.slice(11,16)} -- ${(hora.value / 1000).toFixed(5)} kWh</p>
+									</div>
+								`; */
+
+
+			});
+
+			document.getElementById('lista').innerHTML = preciosHora;
+
+		} else if (respuesta.status === 401) {
+			console.log('Pusiste la llave mal');
+		} else if (respuesta.status === 404) {
+			console.log('Precios no encontrados');
+		} else if (respuesta.status === 502) {
+			lista.innerHTML = 'No hay datos para los filtros seleccionados.'
+			console.log('No hay datos para los filtros seleccionados.');
+		} else {
+
+			console.log('Hubo un error y no sabemos que paso');
+		}
+
+	} catch (error) {
+		console.log(error);
+	}
+
+}
+
+//cargarPrecios();
